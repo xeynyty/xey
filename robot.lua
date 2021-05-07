f6c8c66b476857ec83d625d121acabd369138268
xeynyty <50880754+xeynyty@users.noreply.github.com>
2021-05-07 16:00:28 +0300

    Obsidian robot

--- robot.lua
+++ robot.lua
@@ -1,1 +1,20 @@
-print("Hi, i am robot!")
+local r = require("robot")
+local c = require("component")
+local inv = c.inventory_controller
+
+while true do
+
+    _, type = r.detect()
+
+    if type == "solid" then
+        r.swing()
+    else
+        r.turnAround()
+        inv.equip()
+        r.use()
+        r.turnAround()
+        r.use()
+        inv.equip()
+    end
+
+end
